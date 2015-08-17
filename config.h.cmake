bde259c20044c3d9685fb61244e726d1c1cd7793
Samuel Burbulla <sambu@posteo.de>
2015-08-17 12:36:55 +0200

    inital commit with empty module

---
+++ config.h.cmake
@@ -0,0 +1,45 @@
+/* begin dune-vof
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of dune-vof */
+#define DUNE_VOF_VERSION "@DUNE_VOF_VERSION@"
+
+/* Define to the major version of dune-vof */
+#define DUNE_VOF_VERSION_MAJOR @DUNE_VOF_VERSION_MAJOR@
+
+/* Define to the minor version of dune-vof */
+#define DUNE_VOF_VERSION_MINOR @DUNE_VOF_VERSION_MINOR@
+
+/* Define to the revision of dune-vof */
+#define DUNE_VOF_VERSION_REVISION @DUNE_VOF_VERSION_REVISION@
+
+/* end dune-vof
+   Everything below here will be overwritten
+*/
---
+++ dune/vof/vof.hh
@@ -0,0 +1,6 @@
+#ifndef DUNE_VOF_HH
+#define DUNE_VOF_HH
+
+// add your classes here
+
+#endif // DUNE_VOF_HH
