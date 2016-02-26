a5bc05bb5677c5df5550f6bc4e134704c1cdb3f1
Janick Gerstenberger <janick.gerstenberger@gmail.com>
2016-02-26 12:19:24 +0100

    preliminary concept for a generic polytope (incomplete)

--- dune/vof/geometry/polytope.hh
+++ dune/vof/geometry/polytope.hh
@@ -1,21 +1,147 @@
-#ifndef DUNE_VOF_GEOMETRY_POLYTOPE_HH
-#define DUNE_VOF_GEOMETRY_POLYTOPE_HH
+#ifndef DUNE_VOF_GEOMETRY_POLYTOP_HH
+#define DUNE_VOF_GEOMETRY_POLYTOP_HH
 
 #include <cassert>
 #include <vector>
 
 #include <dune/common/deprecated.hh>
 
+// TODO:
+//  - concretise interface of AlgebraicPolytope / Polytope
+//  - add datat representation and implementation
+// (- remove Polygon2d )
 
 namespace Dune {
 
   namespace VoF {
 
+    // forward declarations
+    // --------------------
+
+    template< int > class AlgebraicPolytope;
+    template< class, int > class Polytope;
+
+
+    // AlgebraicPolytop
+    // ----------------
+
+    template< int dim >
+    class AlgebraicPolytope
+    {
+      using This = AlgebraicPolytope< dim >;
+      static_assert( dim >= 0, "Dimension must be >= 0." );
+
+    public:
+      static constexpr int dimension = dim;
+
+      template< int codim >
+      struct Codim
+      {
+        using Polytope = AlgebraicPolytope< dim-codim >;
+      };
+
+      // constructors ?
+      // ...
+
+      int size ( int codim = 0 ) const
+      {
+        DUNE_THROW( NotImplemented, "size( ... ) not yet imeplemented." )
+        return 0;
+      }
+
+      template< int codim >
+      const typename Codim< codim >::Polytope& subPolytope ( int i ) const
+      {
+        assert( i < size( cdoim ) );
+      }
+
+    private:
+      // data members ... ?
+      // ...
+    };
+
+
+    // Polytop
+    // -------
+
+    template< class Coord, int dim >
+    class Polytope
+    {
+      using This = Polytope< Coord, dim >;
+      static_assert( dim >= 0, "Dimension must be >= 0." );
+
+    public:
+      template< int codim >
+      struct Codim {
+        using Topology = AlgebraicPolytope< dim - codim >;
+        using Polytope  = Polytope< Coord, dim-codim >;
+      };
+      using Topology = typename Codim< 0 >::Topology;
+
+      using Coordinate = Coord;
+      using ctype = typename Coordinate::value_type;
+      using Container = std::vector< Coordinate >;
+
+      static constexpr int dimension = dim;
+      static constexpr int dimensionworld = Coordinate::dimension;
+
+      Polytope ( const Topology& topology, const Container& vertices )
+      : topology_( topology ), vertices_( vertices.begin(), vertices.end() )
+      {
+        assert( size( 0 ) == vertices().size() );
+      };
+
+      int size ( int codim = 0 ) const { return topology().size( codim ); }
+      const Topology& topology () const { return topology_; }
+
+      // subTopology, subPolytope ... ?
+      // ...
+
+      const Coordinate& vertex ( int i ) const
+      {
+        assert( i < size( 0 ) );
+        return vertices_[ i ];
+      }
+
+      ctype volume () const
+      {
+        DUNE_THROW( NotImplemented, "volume() not yet immplented" );
+        return 0.0;
+      }
+
+      Coordinate centroid () const
+      {
+        DUNE_THROW( NotImplemented, " cnetroid() Not yet immplented" );
+        return 0.0;
+      }
+
+      // old interface ...
+      // -----------------
+
+      int corners () const DUNE_DEPRECATED_MSG( "Use size( 0 ) instead." ) { return topology().size( 0 ); }
+
+      const Coordinate& operator[] ( int i ) const DUNE_DEPRECATED_MSG( "Use vertex( ... ) and topology information instead." )
+      {
+        return vertices_[ i % vertices_.size() ];
+      }
+
+      void addVertex ( const Coordinate&, bool = false, double = 1e-12 ) DUNE_DEPRECATED_MSG( "Functionality removed from interface." ) {}
+      void clear () DUNE_DEPRECATED_MSG( "Functionality removed from interface." ) {}
+
+    private:
+      const Container& vertices ()  const { return vertices_; }
+
+      const Topology& topology_;
+      Container vertices_;
+    };
+
+
+
      // OLD CODE / INTERFACE
 
      // konvexes 2D-Polygon
     template< class DomainVector >
-    struct DUNE_DEPRECATED_MSG( "Use Polytope when it is implemented.") Polygon2D
+    struct DUNE_DEPRECATED_MSG( "Use Polytope< ... > when it is implemented." ) Polygon2D
     {
       Polygon2D< DomainVector >() {}
 
@@ -75,4 +201,4 @@ namespace Dune {
 
 } // namespace Dune
 
-#endif // #ifndef DUNE_VOF_GEOMETRY_POLYTOPE_HH
+#endif // #ifndef DUNE_VOF_GEOMETRY_POLYTOP_HH
