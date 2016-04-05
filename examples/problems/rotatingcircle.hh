0ce69eafc18ef91bc1275d4b918119a871f88f5a
Samuel Burbulla <sambu@posteo.de>
2016-04-05 14:28:35 +0200

    Add 3d specialization of rotating circle problem.

--- examples/problems/rotatingcircle.hh
+++ examples/problems/rotatingcircle.hh
@@ -6,12 +6,16 @@
 
 
 template < class ctype, int dim >
-struct RotatingCircle
+struct RotatingCircle {};
+
+
+template < class ctype >
+struct RotatingCircle < ctype, 2 >
 {
-  using DomainType = Dune::FieldVector< ctype, dim >;
+  using DomainType = Dune::FieldVector< ctype, 2 >;
   using RangeType = Dune::FieldVector< ctype, 1 >;
 
-  enum { dimDomain = dim };
+  enum { dimDomain = 2 };
   enum { dimRange = 1 };
 
   void evaluate ( const DomainType &x, RangeType &u ) const
@@ -34,6 +38,48 @@ struct RotatingCircle
   {
     rot = x;
     rot -= DomainType{ 0.5, 0.5 };
+
+    std::swap( rot[ 0 ], rot[ 1 ] );
+    rot[ 1 ] = -rot[ 1 ];
+    rot *= 2 * M_PI / 10;
+  }
+
+  static double maxVelocity() { return 2 * M_PI / 10 * sqrt( 2.0 ); };
+
+};
+
+
+
+
+template < class ctype >
+struct RotatingCircle < ctype, 3 >
+{
+  using DomainType = Dune::FieldVector< ctype, 3 >;
+  using RangeType = Dune::FieldVector< ctype, 1 >;
+
+  enum { dimDomain = 3 };
+  enum { dimRange = 1 };
+
+  void evaluate ( const DomainType &x, RangeType &u ) const
+  {
+    evaluate( x, 0.0, u );
+  }
+
+  void evaluate ( const DomainType &x, double t, RangeType &u ) const
+  {
+    DomainType center{ 0.5, 0.5, 0.5 };
+    center.axpy( std::cos( (2 * M_PI / 10)*t ), DomainType{ 0.0,  0.25, 0.0 } );
+    center.axpy( std::sin( (2 * M_PI / 10)*t ), DomainType{ 0.25,  0.0, 0.0 } );
+
+    double dist = ( x - center ).two_norm();
+
+    u = ( dist < 0.15 ) ? RangeType( 1.0 ) : RangeType( 0.0 );
+  }
+
+  void velocityField ( const DomainType &x, const double t, DomainType &rot ) const
+  {
+    rot = x;
+    rot -= DomainType{ 0.5, 0.5, x[2] };
 
     std::swap( rot[ 0 ], rot[ 1 ] );
     rot[ 1 ] = -rot[ 1 ];
