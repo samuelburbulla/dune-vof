81a63f3fa5bcc78041dcbc91d4cff101f0e0968c
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-12-21 17:30:32 +0100

    initial implementation of an entity

--- dune/vof/interfacegrid/entity.hh
+++ dune/vof/interfacegrid/entity.hh
@@ -2,6 +2,7 @@
 #define DUNE_VOF_INTERFACEGRID_ENTITY_HH
 
 #include <type_traits>
+#include <utility>
 
 #include <dune/grid/common/exceptions.hh>
 #include <dune/grid/common/entity.hh>
@@ -12,343 +13,180 @@ namespace Dune
   namespace VoF
   {
 
-    // External Forward Declarations
-    // -----------------------------
+    // InterfaceGridEntity
+    // -------------------
 
-    template< class, class > class InterfaceGridIntersection;
-    template< class, class > class InterfaceGridIntersectionIterator;
-
-    template< class, class > class InterfaceGridIterator;
-
-
-    // InterfaceGridEntityBasic
-    // -----------------
-
-    /** \copydoc InterfaceGridEntity
-     *
-     *  \nosubgrouping
-     */
-    template< int codim, int dim, class Grid >
-    class InterfaceGridEntityBasic
+    template< int cd, int dim, class Grid >
+    class InterfaceGridEntity< cd, dim, Grid >
     {
-    protected:
+      typedef InterfaceGridEntity< cd, dim, Grid > This;
+
       typedef typename std::remove_const< Grid >::type::Traits Traits;
 
+      static_assert( dim == Traits::dimension, "Internal Dune Error" );
+
     public:
-      /** \name Attributes
-       *  \{ */
-
-      //! codimensioon of the entity
-      static const int codimension = codim;
-      //! dimension of the grid
+      static const int codimension = cd;
       static const int dimension = Traits::dimension;
-      //! dimension of the entity
       static const int mydimension = dimension - codimension;
-      //! dimension of the world
-      static const int dimensionworld = Traits::dimensionworld;
 
-      /** \} */
-
-      /** \name Types Required by DUNE
-       *  \{ */
-
-      //! coordinate type of the grid
-      typedef typename Traits::ctype ctype;
-
-      //! type of corresponding entity seed
-      typedef typename Grid::template Codim< codimension >::EntitySeed EntitySeed;
-      //! type of corresponding geometry
+      typedef typename Traits::template Codim< codimension >::EntitySeed EntitySeed;
       typedef typename Traits::template Codim< codimension >::Geometry Geometry;
 
-      /** \} */
+      typedef typename Traits::Reconstruction::GridView::template Codim< 0 >::Entity HostElement;
 
-    protected:
-      // type of the host grid
-      typedef typename Traits::HostGrid  HostGrid;
+      InterfaceGridEntity () = default;
 
-      // type of extra data, e.g. a pointer to grid (here empty)
-      typedef typename Traits::ExtraData ExtraData;
-
-      // type of entity implementation
-      typedef typename Traits::template Codim< codimension > :: EntityImpl  EntityImpl;
-
-    public:
-      /** \name Host Types
-       *  \{ */
-
-      //! type of corresponding host entity
-      typedef typename HostGrid::template Codim< codimension >::Entity HostEntity;
-      /** \} */
-
-      /** \name Construction, Initialization and Destruction
-       *  \{ */
-
-      /** \brief construct a null entity */
-      explicit InterfaceGridEntityBasic ( ExtraData data )
-      : hostEntity_(),
-        data_ ( data )
+      InterfaceGridEntity ( const HostElement &hostElement, int subEntity )
+        : hostElement_( hostElement ), subEntity_( subEntity )
       {}
 
-      /** \brief construct an initialized entity
-       *
-       *  \param[in]  hostEntity  corresponding entity in the host grid
-       *
-       *  \note The reference to the host entity must remain valid  as long as
-       *        this entity is in use.
-       */
-      InterfaceGridEntityBasic ( ExtraData data, const HostEntity &hostEntity )
-      : hostEntity_( hostEntity ),
-        data_( data )
+      InterfaceGridEntity ( HostElement &&hostElement, int subEntity )
+        : hostElement_( std::move( hostElement ) ), subEntity_( subEntity )
       {}
 
-      /** \} */
+      bool equals ( const This &other ) const { return (hostElement() == other.hostElement()) && (subEntity() == other.subEntity()); }
 
-      /** \brief return true if entity hold a vaild host entity */
-      // operator bool () const { return bool( hostEntity_ ); }
-
-      /** \name Methods Shared by Entities of All Codimensions
-       *  \{ */
-
-      /** \brief obtain the name of the corresponding reference element
-       *
-       *  This type can be used to access the DUNE reference element.
-       */
-      GeometryType type () const
-      {
-        return hostEntity().type();
-      }
-
-      /** \brief obtain the level of this entity */
-      int level () const
-      {
-        return hostEntity().level();
-      }
-
-      /** \brief obtain the partition type of this entity */
-      PartitionType partitionType () const
-      {
-        return hostEntity().partitionType();
-      }
-
-      /** obtain the geometry of this entity */
       Geometry geometry () const
       {
-        return Geometry( hostEntity().geometry() );
+        // TODO: Please implement me
       }
 
-      /** \brief return EntitySeed of host grid entity */
-      EntitySeed seed () const { return typename EntitySeed::Implementation( hostEntity().seed() ); }
+      int level () const { return 0; }
 
-      bool equals( const EntityImpl& other ) const { return hostEntity() ==  other.hostEntity(); }
+      PartitionType partitionType () const { return hostElement().partitionType(); }
 
-      /** \} */
-
-
-      /** \name Methods Supporting the Grid Implementation
-       *  \{ */
-
-      const HostEntity &hostEntity () const
+      EntitySeed seed () const
       {
-        return hostEntity_;
+        // TODO: Please implement me
       }
 
-      ExtraData data() const { return data_; }
+      unsigned int subEntities ( unsigned int codim )
+      {
+        assert( (codim >= static_cast< unsigned int >( codimension )) && (codim <= static_cast< unsigned int >( dimension )) );
+        return 1;
+      }
 
-      /** \} */
+      GeometryType type () const { return GeometryType( (mydimension < 2 ? GeometryType::cube, GeometryType::none), mydimension ); }
 
-    protected:
-      HostEntity hostEntity_;
-      ExtraData        data_;
+      const HostElement &hostElement () const { return hostElement_; }
+
+      int subEntity () const { return subEntity_; }
+
+    private:
+      HostElement hostElement_;
+      int subEntity_;
     };
 
-
-
-    // InterfaceGridEntity
-    // ------------
-
-    template< int codim, int dim, class Grid >
-    class InterfaceGridEntity : public InterfaceGridEntityBasic< codim, dim, Grid >
-    {
-      typedef InterfaceGridEntityBasic< codim, dim, Grid > Base ;
-    protected:
-      typedef typename std::remove_const< Grid >::type::Traits Traits;
-
-    protected:
-      // type of the host grid
-      typedef typename Traits::HostGrid  HostGrid;
-
-      // type of extra data, e.g. a pointer to grid (here empty)
-      typedef typename Traits::ExtraData ExtraData;
-
-    public:
-      using Base :: codimension ;
-
-      /** \name Host Types
-       *  \{ */
-
-      //! type of corresponding host entity
-      typedef typename HostGrid::template Codim< codimension >::Entity HostEntity;
-      /** \} */
-
-      explicit InterfaceGridEntity ( ExtraData data )
-      : Base( data )
-      {}
-
-      InterfaceGridEntity ( ExtraData data, const HostEntity &hostEntity )
-      : Base( data, hostEntity )
-      {}
-    };
 
 
     // InterfaceGridEntity for codimension 0
-    // ----------------------------------
+    // -------------------------------------
 
-    /** \copydoc InterfaceGridEntity
-     *
-     *  \nosubgrouping
-     */
     template< int dim, class Grid >
-    class InterfaceGridEntity< 0, dim, Grid > : public InterfaceGridEntityBasic< 0, dim, Grid >
+    class InterfaceGridEntity< 0, dim, Grid >
     {
-      typedef InterfaceGridEntityBasic< 0, dim, Grid > Base ;
-    protected:
-      typedef typename Base::Traits Traits;
-      typedef typename Base::HostGrid HostGrid;
+      typedef InterfaceGridEntity< cd, dim, Grid > This;
 
-      // type of extra data, e.g. a pointer to grid (here empty)
-      typedef typename Base::ExtraData ExtraData;
+      typedef typename std::remove_const< Grid >::type::Traits Traits;
+
+      static_assert( dim == Traits::dimension, "Internal Dune Error" );
 
     public:
-      using Base::codimension ;
-      using Base::data ;
-      using Base::hostEntity ;
-      /** \name Host Types
-       *  \{ */
+      static const int codimension = 0;
+      static const int dimension = Traits::dimension;
+      static const int mydimension = dimension - codimension;
 
-      //! type of corresponding host entity
-      typedef typename HostGrid::template Codim< codimension >::Entity HostEntity;
-      /** \} */
-
-    protected:
-      typedef typename Traits :: LeafIntersectionIteratorImpl  LeafIntersectionIteratorImpl;
-      typedef typename Traits :: LevelIntersectionIteratorImpl LevelIntersectionIteratorImpl;
-
-      typedef typename Traits :: LeafIntersectionImpl          LeafIntersectionImpl;
-      typedef typename Traits :: LevelIntersectionImpl         LevelIntersectionImpl;
-
-    public:
-      /** \name Types Required by DUNE
-       *  \{ */
-
-      //! type of corresponding local geometry
+      typedef typename Traits::template Codim< codimension >::EntitySeed EntitySeed;
       typedef typename Traits::template Codim< codimension >::LocalGeometry LocalGeometry;
-
-      //! type of Entity interface
-      typedef typename Traits ::template Codim< codimension >::Entity Entity;
-
-      //! type of hierarchic iterator
-      typedef typename Traits::HierarchicIterator        HierarchicIterator;
-      //! type of leaf intersection iterator
-      typedef typename Traits::LeafIntersectionIterator  LeafIntersectionIterator;
-      //! type of level intersection iterator
-      typedef typename Traits::LevelIntersectionIterator LevelIntersectionIterator;
-
-      /** \} */
-
-      /** \name Construction, Initialization and Destruction
-       *  \{ */
-
-      /** \brief construct a null entity */
-      explicit InterfaceGridEntity ( ExtraData data )
-      : Base( data )
-      {}
-
-      /** \brief construct an initialized entity
-       *
-       *  \param[in]  hostEntity  corresponding entity in the host grid
-       *
-       *  \note The reference to the host entity must remain valid as long as
-       *        this entity is in use.
-       */
-      InterfaceGridEntity ( ExtraData data, const HostEntity &hostEntity )
-      : Base( data, hostEntity )
-      {}
-
-      /** \} */
-
-      unsigned int subEntities( const unsigned int codim ) const
-      {
-        return hostEntity().subEntities( codim );
-      }
+      typedef typename Traits::template Codim< codimension >::Geometry Geometry;
 
       template< int codim >
-      int count () const
+      struct Codim
       {
-        return hostEntity().template count< codim >();
+        typedef Dune::Entity< codim, dimension, Grid, InterfaceGridEntity > Entity;
       }
 
-      template< int codim >
-      typename Grid::template Codim< codim >::Entity
-      subEntity ( int i ) const
+      typedef typename Traits::HierarchicIterator HierarchicIterator;
+
+      typedef typename Traits::Reconstruction::GridView::template Codim< 0 >::Entity HostElement;
+
+      InterfaceGridEntity () = default;
+
+      explicit InterfaceGridEntity ( const HostElement &hostElement )
+        : hostElement_( hostElement )
+      {}
+
+      explicit InterfaceGridEntity ( HostElement &&hostElement )
+        : hostElement_( std::move( hostElement ) )
+      {}
+
+      bool equals ( const This &other ) const { return (hostElement() == other.hostElement()) && (subEntity() == other.subEntity()); }
+
+      typename Codim< 0 >::Entity father () const { DUNE_THROW( GridError, "InterfaceGrid consists of only one level" ); }
+
+      Geometry geometry () const
       {
-        typedef typename Traits::template Codim< codim >::EntityImpl EntityImpl;
-        return EntityImpl( data(), hostEntity().template subEntity< codim >( i ) );
+        // TODO: Please implement me
       }
 
-      LevelIntersectionIterator ilevelbegin () const
-      {
-        return LevelIntersectionIteratorImpl( data(), hostEntity().ilevelbegin() );
-      }
+      LocalGeometry geometryInFather () const { DUNE_THROW( GridError, "InterfaceGrid consists of only one level" ); }
 
-      LevelIntersectionIterator ilevelend () const
-      {
-        return LevelIntersectionIteratorImpl( data(), hostEntity().ilevelend() );
-      }
-
-      LeafIntersectionIterator ileafbegin () const
-      {
-        return LeafIntersectionIteratorImpl( data(), hostEntity().ileafbegin() );
-      }
-
-      LeafIntersectionIterator ileafend () const
-      {
-        return LeafIntersectionIteratorImpl( data(), hostEntity().ileafend() );
-      }
-
-      bool hasBoundaryIntersections () const
-      {
-        return hostEntity().hasBoundaryIntersections();
-      }
-
-      bool isLeaf () const
-      {
-        return hostEntity().isLeaf();
-      }
-
-      Entity father () const { DUNE_THROW( GridError, "InterfaceGrid consists of only one level" ); }
-
+      bool hasBoundaryIntersections () const { return true; }
       bool hasFather () const { return false; }
-
-      LocalGeometry geometryInFather () const
-      {
-        return LocalGeometry( hostEntity().geometryInFather() );
-      }
 
       HierarchicIterator hbegin ( int maxLevel ) const
       {
-        typedef typename Traits :: HierarchicIteratorImpl HierarchicIteratorImpl ;
-        return HierarchicIteratorImpl( data(), hostEntity().hbegin( maxLevel ) );
+        // TODO: Please implement me
       }
 
       HierarchicIterator hend ( int maxLevel ) const
       {
-        typedef typename Traits :: HierarchicIteratorImpl HierarchicIteratorImpl ;
-        return HierarchicIteratorImpl( data(), hostEntity().hend( maxLevel ) );
+        // TODO: Please implement me
       }
 
-      bool isRegular () const { return true; }
+      bool isLeaf () const { return true; }
       bool isNew () const { return false; }
+      bool isRegular () const { return true; }
       bool mightVanish () const { return false; }
+
+      int level () const { return 0; }
+
+      PartitionType partitionType () const { return hostElement().partitionType(); }
+
+      EntitySeed seed () const
+      {
+        // TODO: Please implement me
+      }
+
+      template< int codim >
+      typename Codim< codim >::Entity subEntity ( int i ) const
+      {
+        assert( (i >= 0) && (i < static_cast< int >( subEntities( codim ) ) );
+        return subEntity( i, Dune::Codim< codim >() );
+      }
+
+      unsigned int subEntities ( unsigned int codim )
+      {
+        // TODO: extend to (dimension == 2), i.e., the 3d case
+        assert( (codim >= static_cast< unsigned int >( codimension )) && (codim <= static_cast< unsigned int >( dimension )) );
+        return (codim+1);
+      }
+
+      GeometryType type () const { return GeometryType( (mydimension < 2 ? GeometryType::cube, GeometryType::none), mydimension ); }
+
+      const HostElement &hostElement () const { return hostElement_; }
+
+    private:
+      typename Codim< 0 >::Entity subEntity ( int i, Dune::Codim< 0 > ) const { return *this; }
+
+      template< int codim >
+      typename Codim< codim >::Entity subEntity ( int i, Dune::Codim< codim > ) const
+      {
+        return InterfaceGridEntity< codim, dimension, Grid >( hostElement(), i );
+      }
+
+      HostElement hostElement_;
     };
 
   } // namespace VoF
