ca24187e016cfdd60d50b15e8e2176c1ba693543
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-01-18 07:05:52 +0100

    Add mapper for mixed cells only

---
+++ dune/vof/mixedcellmapper.hh
@@ -0,0 +1,96 @@
+#ifndef DUNE_VOF_MIXEDCELLMAPPER_HH
+#define DUNE_VOF_MIXEDCELLMAPPER_HH
+
+#include <cassert>
+#include <cstddef>
+
+#include <limits>
+#include <vector>
+
+#include <dune/grid/common/mcmgmapper.hh>
+
+namespace Dune
+{
+
+  namespace VoF
+  {
+
+    // MixedCellMapper
+    // ---------------
+
+    template< class GridView >
+    class MixedCellMapper
+    {
+      typedef MultipleCodimMultipleGeomTypeMapper< GridView, MCMGElementLayout > ElementMapper;
+
+      typedef typename ElementMapper::Index ElementIndex;
+
+    public:
+      static const int dimension = GridView::dimension;
+
+      typedef std::size_t Index;
+
+      explicit MixedCellMapper ( const Flags< GridView > &flags )
+        : elementMapper_( flags.gridView() ), indices_( elementMapper_.size() )
+      {
+        update( flags );
+      }
+
+      template< class Entity >
+      Index index ( const Entity &entity ) const
+      {
+        const ElementIndex elementIndex = elementMapper_.index( entity );
+        assert( indices_[ elementMapper_.index( entity ) ] != invalidIndex() );
+        return indices_[ elementMapper_.index( entity ) ];
+      }
+
+      template< class Entity >
+      Index subIndex ( const Entity &entity, int i, unsigned int codim ) const
+      {
+        const ElementIndex elementIndex = elementMapper_.subIndex( entity, i, codim );
+        assert( indices_[ elementIndex ] != invalidIndex() );
+        return indices_[ elementIndex ];
+      }
+
+      Index size () const { return size_; }
+
+      template< class Entity >
+      bool contains ( const Entity &entity, Index &index ) const
+      {
+        ElementIndex elementIndex;
+        const bool contains = elementMapper_.contains( entity, elementIndex );
+        index = (contains ? indices_[ elementIndex ] : invalidIndex());
+        return contains;
+      }
+
+      template< class Entity >
+      bool contains ( const Entity &entity, int i, unsigned int codim, Index &index ) const
+      {
+        ElementIndex elementIndex;
+        const bool contains = elementMapper_.contains( entity, i, codim, elementIndex );
+        index = (contains ? indices_[ elementIndex ] : invalidIndex());
+        return contains;
+      }
+
+      void update ( const Flags< GridView > &flags )
+      {
+        elementMapper_.update();
+
+        size_ = 0u;
+        for( const HostElement &element : elements( flags.gridView(), Partition::all ) )
+          indices_[ elementIndex_[ element ] ] = (flags.isMixed( element ) ? size_++ : invalidIndex());
+      }
+
+    private:
+      static constexpr Index invalidIndex () noexcept { return std::numeric_limits< Index >::max(); }
+
+      ElementMapper elementMapper_;
+      std::vector< Index > indices_;
+      Index size_;
+    };
+
+  } // namespace VoF
+
+} // namespace Dune
+
+#endif // #ifndef DUNE_VOF_MIXEDCELLMAPPER_HH
