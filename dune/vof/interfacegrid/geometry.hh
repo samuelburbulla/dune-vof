c606c5a28e3cf8f744e22f92a780c288ad08fd3b
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-01-18 16:05:12 +0100

    implement center and volume for surface polygons

--- dune/vof/interfacegrid/geometry.hh
+++ dune/vof/interfacegrid/geometry.hh
@@ -17,6 +17,106 @@ namespace Dune
   namespace VoF
   {
 
+    // PolygonGeometry
+    // ---------------
+
+    template< class ct, int cdim >
+    struct PolygonGeometry;
+
+    template< class ct, int cdim >
+    struct PolygonGeometry< ct, 3 >
+    {
+      typedef ct ctype;
+      
+      static const int mydimension = 2;
+      static const int coorddimension = 3;
+      
+      typedef FieldVector< ctype, mydimension > LocalCoordinate;
+      typedef FieldVector< ctype, coorddimension > GlobalCoordinate;
+      
+      typedef FieldMatrix< ctype, mydimension, coorddimension > JacobianTransposed;
+      typedef FieldMatrix< ctype, coorddimension, mydimension > JacobianInverseTransposed;
+
+      InterfaceGridGeometry ( const GlobalCoordinate &normal, const GlobalCoordinate *cbegin, std::size_t csize )
+        : normal_( normal ), cbegin_( cbegin ), csize_( csize )
+      {
+        assert( csize >= 3u );
+      }
+
+      int corners () const { return csize_; }
+
+      const GlobalCoordinate &corner ( int i ) const { assert( (i >= 0) && (i < corners) ); return cbegin_[ i ]; }
+
+      GlobalCoordinate center () const
+      {
+        GlobalCoordinate center( 0 );
+        ctype volume = 0;
+        for( std::size_t i = 0; i < csize_; ++i )
+        {
+          const GlobalCoordinate &x = cbegin_[ i ];
+          const GlobalCoordinate &y = cbegin_[ (i+1) % csize_ ];
+          const ctype weight = det( x, y, normal );
+          center.axpy( weight, x + y );
+          volume += weight;
+        }
+        return center *= ctype( 1 ) / (ctype( 3 ) * volume);
+      }
+
+      ctype volume () const
+      {
+        using std::abs;
+
+        ctype volume = 0;
+        for( std::size_t i = 0; i < csize_; ++i )
+          volume += det( cbegin_[ i ], cbegin_[ (i+1) % csize_ ], normal );
+        return abs( volume / ctype( 2 ) );
+      }
+
+      GeometryType type () const noexcept { return GeometryType( GeometryType::none, mydimension ); }
+
+      bool affine () const
+      {
+        DUNE_THROW( InvalidStateException, "Geometry::affine does not make for arbitrary polygons." );
+      }
+
+      GlobalCoordinate global ( const LocalCoordinate &local ) const
+      {
+        DUNE_THROW( InvalidStateException, "Geometry::global does not make for arbitrary polygons." );
+      }
+
+      LocalCoordinate local ( const GlobalCoordinate &global ) const
+      {
+        DUNE_THROW( InvalidStateException, "Geometry::local does not make for arbitrary polygons." );
+      }
+
+      ctype integrationElement ( const LocalCoordinate &local ) const
+      {
+        DUNE_THROW( InvalidStateException, "Geometry::integrationElement does not make for arbitrary polygons." );
+      }
+
+      JacobianTransposed jacobianTransposed ( const LocalCoordinate &local ) const
+      {
+        DUNE_THROW( InvalidStateException, "Geometry::jacobianTransposed does not make for arbitrary polygons." );
+      }
+
+      JacobianInverseTransposed jacobianInverseTransposed ( const LocalCoordinate &local ) const
+      {
+        DUNE_THROW( InvalidStateException, "Geometry::jacobianInverseTransposed does not make for arbitrary polygons." );
+      }
+
+    private:
+      static ctype det ( const GlobalCoordinate &x, const GlobalCoordinate &y, const GlobalCoordinate &z )
+      {
+        return x[ 0 ]*y[ 1 ]*z[ 2 ] + x[ 1 ]*y[ 2 ]*z[ 0 ] + x[ 2 ]*y[ 0 ]*z[ 1 ] - x[ 2 ]*y[ 1 ]*z[ 0 ] - x[ 0 ]*y[ 2 ]*z[ 1 ] - x[ 1 ]*y[ 0 ]*z[ 2 ];
+      }
+
+      GlobalCoordinate normal_;
+      const GlobalCoordinate *cbegin_;
+      std::size_t csize_;
+    }
+
+
+
     // InterfaceGridGeometry
     // ---------------------
 
@@ -59,75 +159,28 @@ namespace Dune
       {
         assert( csize == 2u );
       }
+
+      InterfaceGridGeometry ( const GlobalCoordinate &normal, const GlobalCoordinate *cbegin, std::size_t csize )
+        : This( cbegin, csize )
+      {}
     };
 
     template< int cdim, class Grid >
     class InterfaceGridGeometry< 2, cdim, Grid >
+      : public PolygonGeometry< typename std::remove_const_t< Grid >::Traits::ctype, cdim >
     {
       typedef InterfaceGridGeometry< 2, cdim, Grid > This;
+      typedef PolygonGeometry< typename std::remove_const_t< Grid >::Traits::ctype, cdim > Base;
 
     public:
-      typedef ct ctype;
-      
-      static const int mydimension = 2;
-      static const int coorddimension = cdim;
-      
-      typedef FieldVector< ctype, mydimension > LocalCoordinate;
-      typedef FieldVector< ctype, coorddimension > GlobalCoordinate;
-      
-      typedef FieldMatrix< ctype, mydimension, coorddimension > JacobianTransposed;
-      typedef FieldMatrix< ctype, coorddimension, mydimension > JacobianInverseTransposed;
+      typedef typename Base::ctype ctype;
 
-      InterfaceGridGeometry ( const GlobalCoordinate *cbegin, std::size_t csize )
-        : cbegin_( cbegin ), csize_( csize )
-      {
-        assert( csize >= 3u );
-      }
+      typedef typename Base::GlobalCoordinate GlobalCoordinate;
 
-      int corners () const { return csize_; }
-
-      const GlobalCoordinate &corner ( int i ) const { assert( (i >= 0) && (i < corners) ); return cbegin_[ i ]; }
-
-      GlobalCoordinate center () const { return polygonBaryCenter( rotate_, cbegin_, cbegin_ + csize_ ); }
-
-      ctype volume () const { return polygonVolume( rotate_, cbegin_, cbegin_ + csize_ ); }
-
-      GeometryType type () const noexcept { return GeometryType( GeometryType::none, mydimension ); }
-
-      bool affine () const
-      {
-        DUNE_THROW( InvalidStateException, "Geometry::affine does not make for arbitrary polygons." );
-      }
-
-      GlobalCoordinate global ( const LocalCoordinate &local ) const
-      {
-        DUNE_THROW( InvalidStateException, "Geometry::global does not make for arbitrary polygons." );
-      }
-
-      LocalCoordinate local ( const GlobalCoordinate &global ) const
-      {
-        DUNE_THROW( InvalidStateException, "Geometry::local does not make for arbitrary polygons." );
-      }
-
-      ctype integrationElement ( const LocalCoordinate &local ) const
-      {
-        DUNE_THROW( InvalidStateException, "Geometry::integrationElement does not make for arbitrary polygons." );
-      }
-
-      JacobianTransposed jacobianTransposed ( const LocalCoordinate &local ) const
-      {
-        DUNE_THROW( InvalidStateException, "Geometry::jacobianTransposed does not make for arbitrary polygons." );
-      }
-
-      JacobianInverseTransposed jacobianInverseTransposed ( const LocalCoordinate &local ) const
-      {
-        DUNE_THROW( InvalidStateException, "Geometry::jacobianInverseTransposed does not make for arbitrary polygons." );
-      }
-
-    private:
-      const GlobalCoordinate *cbegin_;
-      std::size_t csize_;
-    }
+      InterfaceGridGeometry ( const GlobalCoordinate &normal, const GlobalCoordinate *cbegin, std::size_t csize )
+        : Base( normal, cbegin, csize )
+      {}
+    };
 
   } // namespace VoF
 
