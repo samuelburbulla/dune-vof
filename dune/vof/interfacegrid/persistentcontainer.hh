ed11f4beff2556af9116d2518fcf362c65964adf
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-01-19 15:55:03 +0100

    implement PersistentContainer for InterfaceGrid

--- dune/vof/interfacegrid/persistentcontainer.hh
+++ dune/vof/interfacegrid/persistentcontainer.hh
@@ -1,28 +1,32 @@
 #ifndef DUNE_VOF_INTERFACEGRID_PERSISTENTCONTAINER_HH
 #define DUNE_VOF_INTERFACEGRID_PERSISTENTCONTAINER_HH
 
-#include <dune/grid/utility/persistentcontainerwrapper.hh>
+#include <vector>
+
+#include <dune/grid/utility/persistentcontainer.hh>
+#include <dune/grid/utility/persistentcontainervector.hh>
 
 #include <dune/vof/interfacegrid/declaration.hh>
+#include <dune/vof/interfacegrid/indexset.hh>
 
 namespace Dune
 {
 
   // PersistentContainer for InterfaceGrid
-  // ------------------------------
+  // -------------------------------------
 
-  template< class HostGrid, class T >
-  class PersistentContainer< InterfaceGrid< HostGrid >, T >
-  : public PersistentContainerWrapper< InterfaceGrid< HostGrid >, T >
+  template< class Reconstruction, class T >
+  class PersistentContainer< VoF::InterfaceGrid< Reconstruction >, T >
+    : public PersistentContainerVector< VoF::InterfaceGrid< Reconstruction >, VoF::InterfaceGridIndexSet< const VoF::InterfaceGrid< Reconstruction > >, std::vector< T > >
   {
-    typedef PersistentContainerWrapper< InterfaceGrid< HostGrid >, T > Base;
+    typedef PersistentContainerVector< VoF::InterfaceGrid< Reconstruction >, VoF::InterfaceGridIndexSet< const VoF::InterfaceGrid< Reconstruction > >, std::vector< T > > Base;
 
   public:
     typedef typename Base::Grid Grid;
     typedef typename Base::Value Value;
 
     PersistentContainer ( const Grid &grid, int codim, const Value &value = Value() )
-    : Base( grid, codim, value )
+      : Base( grid.leafIndexSet(), codim, value )
     {}
   };
 
