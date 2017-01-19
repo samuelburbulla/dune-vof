399810cbdcb3363285ca877f55c7953081b491dd
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-01-19 16:09:14 +0100

    implement HostGridAccess

--- dune/vof/interfacegrid/hostgridaccess.hh
+++ dune/vof/interfacegrid/hostgridaccess.hh
@@ -1,6 +1,8 @@
 #ifndef DUNE_VOF_INTERFACEGRID_HOSTGRIDACCESS_HH
 #define DUNE_VOF_INTERFACEGRID_HOSTGRIDACCESS_HH
 
+#include <type_traits>
+
 #include <dune/grid/common/intersection.hh>
 
 #include <dune/vof/interfacegrid/declaration.hh>
@@ -17,78 +19,66 @@ namespace Dune
 
 
 
-  /** \class HostGridAccess
-   *  \brief provides access to host grid objects
+  /**
+   * \class HostGridAccess
+   * \brief provides access to host grid objects
    *
-   *  \tparam  Grid  meta grid, whose host grid shall be accessed
+   * \tparam  Grid  meta grid, whose host grid shall be accessed
    *
-   *  \nosubgrouping
-   */
-  template< class HG >
-  struct HostGridAccess< InterfaceGrid< HG > >
+   * \nosubgrouping
+   **/
+  template< class R >
+  struct HostGridAccess< InterfaceGrid< R > >
   {
     /** \name Exported Types
-     * \{ */
+     * \{
+     **/
 
-    typedef InterfaceGrid< HG > Grid;
+    typedef VoF::InterfaceGrid< R > Grid;
+
+    const int dimension = Grid::dimension;
 
     //! type of HostGrid
-    typedef typename Grid::HostGrid HostGrid;
+    typedef typename R::GridView::Grid HostGrid;
 
-     /** \} */
+     /** \} **/
 
     /** \brief A Traits struct that collects return types of class member methods.
      *
      *  \tparam codim codimension
-     */
-    template< int codim >
+     **/
+    template< int codim, class T = void >
+    struct Codim;
+
+    template< int codim, std::enable_if_t< (codim == 0) > >
     struct Codim
     {
       //! type of the InterfaceGrid entity
-      typedef typename Grid::template Codim< codim >::Entity Entity;
+      typedef Dune::Entity< codim, dimension, const Grid, VoF::InterfaceGridEntity > Entity;
 
       //! type of the host entity
       typedef typename HostGrid::template Codim< codim >::Entity HostEntity;
     };
 
-    //! type of the InterfaceGrid leaf intersection
-    typedef typename Grid::Traits::LeafIntersection LeafIntersection;
-    //! type of the InterfaceGrid level intersection
-    typedef typename Grid::Traits::LevelIntersection LevelIntersection;
+    template< int codim, std::enable_if_t< (codim > 0) && (codim <= dimension) > >
+    struct Codim
+    {};
 
-    //! type of the host leaf intersection
-    typedef typename HostGrid::Traits::LeafIntersection HostLeafIntersection;
-    //! type of the host level intersection
-    typedef typename HostGrid::Traits::LevelIntersection HostLevelIntersection;
-
-    /** \brief Get underlying HostGrid.
-     *  \param[in] grid  InterfaceGrid
-     *  \returns HostGrid
+    /**
+     * \brief Get underlying HostGrid.
+     * \param[in] grid  InterfaceGrid
+     * \returns HostGrid
      */
     static const HostGrid &hostGrid ( const Grid &grid )
     {
-      return grid.hostGrid();
-    }
-
-    template< class Entity >
-    static const typename Codim< Entity::codimension >::HostEntity &
-    hostEntity ( const Entity &entity )
-    {
-      return hostEntity< Entity::codimension >( entity );
+      return grid.reconstruction().gridView().grid();
     }
 
     template< int codim >
     static const typename Codim< codim >::HostEntity &
-    hostEntity ( const typename Codim< codim >::Entity &entity )
+    hostEntity ( const Dune::Entity< codim, dimension, const Grid, VoF::InterfaceGridEntity > &entity )
     {
-      return Grid::getRealImplementation( entity ).hostEntity();
-    }
-
-    template< class HostIntersection >
-    static const HostIntersection &
-    hostIntersection ( const Intersection< const Grid, InterfaceGridIntersection< const Grid, HostIntersection > > &intersection )
-    {
-      return Grid::getRealImplementation( intersection ).hostIntersection();
+      return Grid::getRealImplementation( entity ).hostElement();
     }
   };
 
